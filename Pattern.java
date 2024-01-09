512abcb78ead8a4b98a89c67994e8090c4d0fabc
Sowmiya <114464938+Sowmiya214@users.noreply.github.com>
2024-01-09 21:01:38 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -7,19 +7,17 @@ public class Pattern {
 	public static void main(String[] args) {
 		Scanner obj = new Scanner(System.in);
 		System.out.println("Enter the number");
-        int n = obj.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j == 0 || j <= i) {
-                    System.out.print(n - j + " ");
-                } else {
-                    System.out.print(n - i + " ");
-                }
-            }
-            System.out.println();
-        }
-
-
+		        int n = obj.nextInt();
+		        for (int i = 0; i < n; i++) {
+		            for (int j = 0; j < n; j++) {
+		                if (j == 0 || j <= i) {
+		                    System.out.print(n - j + " ");
+		                } else {
+		                    System.out.print(n - i + " ");
+		                }
+		            }
+		            System.out.println();
+		        }
 	}
 
 }
